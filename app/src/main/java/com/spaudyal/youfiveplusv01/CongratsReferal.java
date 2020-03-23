55c1bd16f020287388c3b172493e8157dcdaaec0
Saurav Paudyal <spaudyal@gmail.com>
2020-03-23 14:58:36 -0400

    ALL WORKING

--- app/src/main/java/com/spaudyal/youfiveplusv01/CongratsReferal.java
+++ app/src/main/java/com/spaudyal/youfiveplusv01/CongratsReferal.java
@@ -1,6 +1,8 @@
 package com.spaudyal.youfiveplusv01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,20 @@ public class CongratsReferal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congrats_referal);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+               openCongratsBarCode();
+            }
+
+        }, 3000L);
+    }
+
+    protected void openCongratsBarCode(){
+        Intent intent = new Intent(this, CongratsBarCodeDisplay.class);
+        startActivity(intent);
     }
 }
